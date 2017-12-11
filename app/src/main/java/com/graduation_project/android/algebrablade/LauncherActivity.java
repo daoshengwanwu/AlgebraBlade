1d045bb5a8146899da1a128089b1629dc698ca76
baihaoran <baihaoran@xiaomi.com>
2017-12-11 17:55:33 +0800

    初始化项目

---
+++ app/src/main/java/com/graduation_project/android/algebrablade/LauncherActivity.java
@@ -0,0 +1,13 @@
+package com.graduation_project.android.algebrablade;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
