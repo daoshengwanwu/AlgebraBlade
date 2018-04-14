4aae96fc23bb84c83d50f2915ba72591c6c487f8
白浩然 <bai_haoran@outlook.com>
2018-04-14 21:27:55 +0800

    解决回退问题

--- app/src/main/java/com/zhouxiuya/LoginActivity.java
+++ app/src/main/java/com/zhouxiuya/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.graduation_project.android.algebrablade.GraphicEditActivity;
 import com.graduation_project.android.algebrablade.R;
@@ -23,5 +24,19 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                finish();
+                return true;
+            }
+
+            default: break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
