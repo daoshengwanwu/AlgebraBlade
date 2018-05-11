b6ca865919219a513e0847f6e15022aca0325323
李阔 <982619692@qq.com>
2018-05-11 12:42:25 +0800

    添加积分Activity

---
+++ app/src/main/java/com/graduation_project/android/algebrablade/QuadratureActivity.java
@@ -0,0 +1,13 @@
+package com.graduation_project.android.algebrablade;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuadratureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quadrature2);
+    }
+}
